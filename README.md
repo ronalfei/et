f55abc2be15e74fa02beae84be80d814fc68ef97
BlackAnimal <ronalfei@qq.com>
2015-04-17 16:10:23 +0800

    merge pull request

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<<<<<<< HEAD
 # et
 An Easy Tcp Load Test tool
+=======
+et
+===
+##An easy Tcp Load Test tools
+
+*start.sh to start*
+>>>>>>> d00a2acf5d15532f469a332b617b5c2e50cc8041
--- config.erl
+++ config.erl
@@ -1,16 +1,39 @@
 -module(config).
 
--export([get/1]).
+-export([get/1, get/2, init/0, init_ets/1]).
 
+init() ->
+    Result = file:consult("config"),
+    case Result of
+        {ok, Term} ->
+            init_ets(Term);
+        {error, _} ->
+            io:format("[ETS Init fail] Read File Error~n~n"),
+            error;
+        _ ->
+            error
+    end.
 
-get(workers) ->
-	{ok,[[{workers, Workers}, {requests, _Requests}]]} = file:consult("config"),
-	io:format("workers is :~p~n", [Workers]),
-	Workers;
-get(requests) ->
-	{ok,[[{workers, _Workers}, {requests, Requests}]]} = file:consult("config"),
-	io:format("request is :~p~n", [Requests]),
-	Requests;
-get(_) ->
-	{ok,[[{workers, Workers}, {requests, Requests}]]} = file:consult("config"),
-	{Workers, Requests}.
+init_ets(Term) ->
+    try
+        ets:new(config, [named_table, public]),
+        ParentPID = erlang:group_leader(),
+        ets:give_away(config, ParentPID, []),
+        ets:insert(config, Term)
+    catch A:B ->
+		io:format("init ets table warning:~p   + ~p~n", [A, B]),
+		ets:delete_all_objects(config)
+    end.
+
+get(Key) ->
+    Value = ets:lookup(config, Key),
+    get(Key, Value).
+
+get(Key, []) ->
+    none;
+get(Key, [H|T]) ->
+    case H of
+        {Key, Value} -> Value;
+        Other ->
+            get(Key, T)
+    end.
