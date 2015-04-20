0aa4e76f6690e29d5d31ef53482dd053bb2a2831
BlackAnimal <ronalfei@qq.com>
2015-04-20 10:52:28 +0800

    nothing

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-erlc +native +{hipe,[to_llvm]} worker.erl manager.erl stats.erl worker1.erl manager1.erl config.erl collect.erl
+erlc +native +{hipe,[to_llvm]} worker.erl manager.erl stats.erl  config.erl collect.erl
 erl -boot start_sasl -sname test@localhost +K true -P 134217727 -Q 134217727 -env  ERL_MAX_PORTS 409600  -setcookie edu_dp \
 	-s manager \
     -eval "io:format(\"* test service already started~n~n\")."
