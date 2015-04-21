3e08e9bee098e90e1a862ff479ffe6e350811675
BlackAnimal <ronalfei@qq.com>
2015-04-21 20:22:57 +0800

    mv beam to ebin

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 erlc +native +{hipe,[to_llvm]} worker.erl manager.erl stats.erl config.erl collect.erl
+mv *.beam ./ebin/
 erl -boot start_sasl -sname test@localhost +K true -P 134217727 -Q 134217727 -env  ERL_MAX_PORTS 409600  -setcookie edu_dp \
 	-s manager \
+	-pa ebin \
     -eval "io:format(\"* test service already started~n~n\")."
